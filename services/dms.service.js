67a94afe1df6b230d34f65bcce2f89fda24628c7
Anggi Fergian <a.fergian.pratama@accenture.com>
2025-08-29 17:19:16 +0700

    feat: init dms service for upload & download

---
+++ services/dms.service.js
@@ -0,0 +1,31 @@
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
+const { PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
+
+const s3 = require('../utils/s3');
+
+async function generateUploadUrl(filename, mimetype) {
+  const key = `uploads/${Date.now()}-${filename}`;
+
+  const command = new PutObjectCommand({
+    Bucket: process.env.AWS_S3_BUCKET,
+    Key: key,
+    ContentType: mimetype,
+  });
+
+  const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+
+  return { url, key };
+}
+
+async function generateDownloadUrl() {
+  const command = new GetObjectCommand({
+    Bucket: process.env.AWS_S3_BUCKET,
+    Key: fileName,
+  });
+
+  const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+
+  return url;
+}
+
+module.exports = { generateUploadUrl, generateDownloadUrl };
